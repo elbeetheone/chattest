1d00b1b468de10e718a83160ec31e159e487b286
elbeetheone <101788193+elbeetheone@users.noreply.github.com>
2024-05-14 11:23:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,30 @@ llm = Ollama(model="llama3:70b")
 
 st.title("Chatbot using Llama3")
 
+for message in st.session_state.messages:
+    st.chat_message(message["role"]).markdown(message["content"])
+
+
 prompt = st.text_area("Enter your prompt:")
 
-if st.button("Generate"):
-    if prompt:
-        with st.spinner("Generating response..."):
-            st.write_stream(llm.stream(prompt, stop=['<|eot_id|>']))
+if prompt:
+    # st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = client.chat.completions.create(
+        model="llama.cpp/models/mistral-7b-instruct-v0.1.Q4_0.gguf",
+        messages=st.session_state.messages,
+        stream=True,
+    )
+
+    complete_response = ""
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        for chunk in response:
+            if chunk.choices[0].delta.content is not None:
+                complete_response += chunk.choices[0].delta.content
+                message_placeholder.markdown(complete_response + "▌")
+                message_placeholder.markdown(complete_response)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": complete_response}
+    )
