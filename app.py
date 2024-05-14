3734742f4f1e5d9daeef219ab2703344aa7221f0
elbeetheone <101788193+elbeetheone@users.noreply.github.com>
2024-05-14 11:42:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,13 @@
 from langchain_community.llms import Ollama 
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPrompt
 import streamlit as st
-
-client = ChatOpenAI(model_name=llm, stream=True)
 
 llm = Ollama(model="llama3:70b")
 
 st.title("Chatbot using Llama3")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {
-            "role": "system",
-            "content": """
-                You are an intelligent assistant. Help the user.
-
-            """,
-        }
-    ]
-
-
 prompt = st.text_area("Enter your prompt:")
 
-if prompt:
-    # st.chat_message("user").markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    response = client.chat.completions.create(
-        model=llm,
-        messages=st.session_state.messages,
-        stream=True,
-    )
-
-    complete_response = ""
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        for chunk in response:
-            if chunk.choices[0].delta.content is not None:
-                complete_response += chunk.choices[0].delta.content
-                message_placeholder.markdown(complete_response + "▌")
-                message_placeholder.markdown(complete_response)
-    st.session_state.messages.append(
-        {"role": "assistant", "content": complete_response}
-    )
+if st.button("Generate"):
+    if prompt:
+        with st.spinner("Generating response..."):
+            st.write_stream(llm.stream(prompt))
