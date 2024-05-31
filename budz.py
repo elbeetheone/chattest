2dbb911f8f4478a6d84a3a28420a0d9fb5acf6a5
elbeetheone <101788193+elbeetheone@users.noreply.github.com>
2024-05-31 15:43:30 +0100

    Update budz.py

--- budz.py
+++ budz.py
@@ -6,28 +6,29 @@ import json
 import requests
 
 foo = st.query_params.get("foo", ["default_value"])
-user_words = st.query_params.get("user_words", ["default_value"])
-user = st.query_params.get("user", ["default_value"])
+# user_words = st.query_params.get("user_words", ["default_value"])
+# user = st.query_params.get("user", ["default_value"])
+
+st.title(str(foo+1))
+
+# wv = KeyedVectors.load('google_50_vectors.kv')
+
+# def seenonim(user_response):
+#     nu_list = []
+#     today = foo
+#     for num in range(5):
+#         try:
+#             if today[num] == user_response[num].lower():
+#                 nu_list.append(0)
+#             elif difflib.SequenceMatcher(None, today[num], user_response[num].lower()).ratio() > 0.8:
+#                 nu_list.append(0.05)
+#             else:
+#                 nu_list.append(wv.similarity(today[num],user_response[num].lower()))
+#         except Exception as e:
+#             nu_list.append(0)
+#     return nu_list
 
 
-wv = KeyedVectors.load('google_50_vectors.kv')
-
-def seenonim(user_response):
-    nu_list = []
-    today = foo
-    for num in range(5):
-        try:
-            if today[num] == user_response[num].lower():
-                nu_list.append(0)
-            elif difflib.SequenceMatcher(None, today[num], user_response[num].lower()).ratio() > 0.8:
-                nu_list.append(0.05)
-            else:
-                nu_list.append(wv.similarity(today[num],user_response[num].lower()))
-        except Exception as e:
-            nu_list.append(0)
-    return nu_list
-
-
-url = 'https://speakeasi.app/_/api/budzscore'
-myobj = {'today_words': seenonim(user_words), 'user':user}
-requests.post(url, json = myobj)
+# url = 'https://speakeasi.app/_/api/budzscore'
+# myobj = {'today_words': seenonim(user_words), 'user':user}
+# requests.post(url, json = myobj)
