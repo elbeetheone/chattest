3dac48d25dc843ebf112f13ba443dbd543bfada6
elbeetheone <101788193+elbeetheone@users.noreply.github.com>
2024-06-01 06:09:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ wv = KeyedVectors.load('google_50_vectors.kv')
 words = list(wv.key_to_index.keys())
 web2lowerset = get_english_words_set(['web2'], lower=True)
 
+foo = st.query_params.get("foo", ["default_value"])
+
 def paste_budz():
     def has_more_than_five_similars(word, web2lowerset, wv):
         similar_words = [similar_word[0] for similar_word in wv.most_similar(word, topn=10)]
@@ -25,61 +27,7 @@ def paste_budz():
             today_words.append(candidate)
     return today_words
 
-url = 'https://speakeasi.app/_/api/savevideo'
-myobj = {'today_words': paste_budz()}
-requests.post(url, json = myobj)
-
-
-
-# from openai import OpenAI
-# import streamlit as st
-
-
-# hide_st_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
-# st.markdown(hide_st_style, unsafe_allow_html=True)
-
-# client = OpenAI(api_key= st.secrets['OPENAI_API_KEY'])
-
-# # Extract the 'foo' parameter
-# foo = st.query_params.get("foo", ["default_value"])
-
-# if "messages" not in st.session_state:
-#     st.session_state["messages"] = [
-#         {
-#             "role": "system",
-#             "content": st.secrets['PROMPT'],
-#         }
-#     ]
-
-# st.title("🗣️ Ari's Here to help")
-# # for message in st.session_state.messages:
-# #     st.chat_message(message["role"]).markdown(message["content"])
-
-# prompt = foo
-# if prompt:
-#     # st.chat_message("user").markdown(prompt)
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-
-#     response = client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=st.session_state.messages,
-#         stream=True,
-#     )
-
-#     complete_response = ""
-#     with st.chat_message("assistant"):
-#         message_placeholder = st.empty()
-#         for chunk in response:
-#             if chunk.choices[0].delta.content is not None:
-#                 complete_response += chunk.choices[0].delta.content
-#                 message_placeholder.markdown(complete_response + "▌")
-#                 message_placeholder.markdown(complete_response)
-#     st.session_state.messages.append(
-#         {"role": "assistant", "content": complete_response}
-#     )
+if foo == st.secrets['CODE']:
+    url = 'https://speakeasi.app/_/api/savevideo'
+    myobj = {'today_words': paste_budz()}
+    requests.post(url, json = myobj)
