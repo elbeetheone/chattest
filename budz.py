1d0a606a9a0170792b64d0ec4d9e133876d5d87d
Laolu Ibrahim <olaoluwa.ibrahim@gmail.com>
2024-06-19 14:38:35 +0100

    Last changes, hopefilly

--- budz.py
+++ budz.py
@@ -17,17 +17,13 @@ wv = load_model('glove-twitter-50')
 user_words = st.query_params.get("user_words", ["default_value"])
 user = st.query_params.get("user", ["default_value"])
 foo = st.query_params.get("foo", ["default_value"])
-
-
-
-foo_ = foo.split(',')
-user_words_ = user_words.split(',')
+bar = st.query_params.get("bar", ["default_value"])
 
 
 
 def seenonim(user_response):
+    today = foo.split(',')
     nu_list = []
-    today = foo_
     for num in range(5):
         try:
             if today[num] == user_response[num].lower() or  user_response[num] == '_':
@@ -42,8 +38,19 @@ def seenonim(user_response):
             nu_list.append(json.dumps(str(0)))
     return nu_list
 
+def get_transcript(topic, transcript):
+    ratings = abs(1-wv.wmdistance(topic,transcript))
+    #lazy model :)
+    return ratings
 
 
-url = st.secrets['WEB']
-myobj = {'today_words': seenonim(user_words_), 'user':user, 'foo':foo, 'user_words': user_words}
-requests.post(url, json = myobj)
+if bar == st.secrets['BAR_1']:
+    user_words_ = user_words.split(',')
+    url = st.secrets['WEB']
+    myobj = {'today_words': seenonim(user_words_), 'user':user, 'foo':foo, 'user_words': user_words}
+    requests.post(url, json = myobj)
+
+if bar == st.secrets['BAR_2']:
+    url = st.secrets['WEB_2']
+    myobj = {'score': get_transcript(user_words, foo), 'user':user}
+    requests.post(url, json = myobj)
--- rate.py
+++ rate.py
@@ -1,91 +1,27 @@
 import streamlit as st
-from gensim.models import Word2Vec, KeyedVectors
 import gensim.downloader
-import re
-import numpy as np
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize,word_tokenize
-from gensim.models import Word2Vec, KeyedVectors
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import cohen_kappa_score
-from keras.layers import Embedding, LSTM, Dense, Dropout, Lambda, Flatten
-import tensorflow as tf
-from tensorflow.keras.models import Sequential, load_model, model_from_config
-from keras import backend as K
+import json
+import requests
 
+@st.cache_resource
+def load_model(name):
+    model_wiki = gensim.downloader.load(name)
+    return model_wiki
 
+wv = load_model('glove-twitter-50')
 
 topic = st.query_params.get("topic", ["default_value"])
 user = st.query_params.get("user", ["default_value"])
 trans = st.query_params.get("trans", ["default_value"])
 
 
-def sent2word(x):
-    stop_words = set(stopwords.words('english'))
-    x=re.sub("[^A-Za-z]"," ",x)
-    x.lower()
-    filtered_sentence = []
-    words=x.split()
-    for w in words:
-        if w not in stop_words:
-            filtered_sentence.append(w)
-    return filtered_sentence
 
-def essay2word(essay):
-    essay = essay.strip()
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    raw = tokenizer.tokenize(essay)
-    final_words=[]
-    for i in raw:
-        if(len(i)>0):
-            final_words.append(sent2word(i))
-    return final_words
-
-def makeVec(words, model, num_features):
-    vec = np.zeros((num_features,),dtype="float32")
-    noOfWords = 0.
-    index2word_set = set(model.index_to_key)
-    for i in words:
-        if i in index2word_set:
-            noOfWords += 1
-            vec = np.add(vec,model[i])
-    vec = np.divide(vec,noOfWords)
-    return vec
-
-def getVecs(essays, model, num_features):
-    c=0
-    essay_vecs = np.zeros((len(essays),num_features),dtype="float32")
-    for i in essays:
-        essay_vecs[c] = makeVec(i, model, num_features)
-        c+=1
-    return essay_vecs
-
-@st.cache_resource
-def convertToVec(text):
-    content=text
-    if len(content) > 20:
-        num_features = 300
-        model = KeyedVectors.load_word2vec_format("word2vecmodel.bin", binary=True)
-        clean_test_essays = []
-        clean_test_essays.append(sent2word(content))
-        testDataVecs = getVecs(clean_test_essays, model, num_features )
-        testDataVecs = np.array(testDataVecs)
-        testDataVecs = np.reshape(testDataVecs, (testDataVecs.shape[0], 1, testDataVecs.shape[1]))
-
-        lstm_model = load_model("essay.h5")
-        preds = lstm_model.predict(testDataVecs)
-        return preds[0][0] * 10
 
 @st.cache_resource
 def get_transcript(topic, transcript):
-    wv = gensim.downloader.load('glove-wiki-gigaword-50')
-    ratings_1 = convertToVec(transcript) * 0.80
-    ratings_2 = abs(1-wv.wmdistance(topic,transcript)) * 0.20
-    Content_ratings = ratings_1 + ratings_2
-
-    return Content_ratings
+    ratings = abs(1-wv.wmdistance(topic,transcript))
+    #lazy model :)
+    return ratings
 
 
 url = st.secrets['WEB']
