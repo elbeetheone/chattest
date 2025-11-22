7ec3f5db65e99623343d362899f0a41f0e96bff8
elbeetheone <elbeetheone@github.com>
2025-11-22 12:58:55 -0500

    added new secrets

--- budz.py
+++ budz.py
@@ -281,6 +281,7 @@ if bar == st.secrets['BAR_4']: #ace_it
 if bar == st.secrets['BAR_5']: # kaawe
     class_ = foo
     user = user
+    myobj = {'user':user, 'class_':class_}
     passage = requests.get('https://svelte-possible-work.anvil.app/_/get_passages', json=myobj)
     comp_score = kaawe(passage[0], passage[-1])
     url = st.secrets['WEB_6']
